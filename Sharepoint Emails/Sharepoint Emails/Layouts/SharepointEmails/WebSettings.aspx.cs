5955951153e8df4b3c2c7509e8554805e508757b
Vitaly2007_cp <SND\Vitaly2007_cp>
2012-05-31 10:27:29 +0000

    added link to web setings

---
+++ Sharepoint Emails/Sharepoint Emails/Layouts/SharepointEmails/WebSettings.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+
+namespace SharepointEmails.Layouts.SharepointEmails
+{
+    public partial class WebSettings : LayoutsPageBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
